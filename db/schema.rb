cc4fa71ef1e187b3918f09f3fef854f35ca824a8
Jacob Jackson <jacob.t.jackson@gmail.com>
2015-12-13 15:41:17 -0600

    checkpoint-48 api completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209035726) do
+ActiveRecord::Schema.define(version: 20151213045211) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -126,6 +126,7 @@ ActiveRecord::Schema.define(version: 20151209035726) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
